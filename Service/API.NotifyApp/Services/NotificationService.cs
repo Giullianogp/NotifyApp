e966570d26d525188163f7b9f2cd871e2d7d2e1c
Giulliano G. Pivetta <giullianogp@hotmail.com>
2018-05-04 20:13:43 -0300

    Adiciona hub

---
+++ Service/API.NotifyApp/Services/NotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.NotifyApp.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public void SendNotification()
+        {
+            
+        }
+    }
+}
